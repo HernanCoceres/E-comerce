f0f36f21e4f99551758fe899c72123d9947d1fd1
HernanCoceres <HernanCoceres@outlook.com>
2024-12-19 10:55:36 -0300

    Corrigiendo la funcionalidad de buy

--- E-comerce/src/CheckOutFormDate.jsx
+++ E-comerce/src/CheckOutFormDate.jsx
@@ -1,27 +1,26 @@
 import React, { useState } from "react"
 import FormDate from "./FormDate"
 
-function CheckOutFormDate({clearCart}) {
-  const [dataForm, setDataForm] = useState({
-    name: "",
-    age: "",
-    address: "",
-    paymentMethod: "efectivo",
-  })
+function CheckOutFormDate({ clearCart }) {
+  const [dataForm, setDataForm] = useState({ name: "", age: "", address: "", paymentMethod: "cash",})
 
   const resetForm = () => {
-    setDataForm({
-      name: "",
-      age: "",
-      address: "",
-      paymentMethod: "efectivo",
-    });
+    setDataForm({ name: "", age: "", address: "", paymentMethod: "cash",})
     clearCart()
   }
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setDataForm((prevData) => ({...prevData, [name]: value,}))
+  }
+
 
   return (
-    <FormDate clearCart={resetForm}/>
-  )
+    <FormDate
+      clearCart={resetForm}
+      dataForm={dataForm}
+      setDataForm={setDataForm}
+      handleChange={handleChange}
+    />)
 }
 
 export default CheckOutFormDate
--- E-comerce/src/FormDate.jsx
+++ E-comerce/src/FormDate.jsx
@@ -1,32 +1,30 @@
 import { Form, Button } from "react-bootstrap"
-import styles from './Modules/FormDate.module.css'
+import styles from "./Modules/FormDate.module.css"
 
-function FormDate ({clearCart}) {
-    return(
+function FormDate({ clearCart, dataForm, setDataForm, handleChange }) {
+
+  return (
     <Form className={styles.form}>
       <Form.Group controlId="name" className="mb-3">
-        <Form.Control type="text" name="name" placeholder="Enter your name"/>
-      </Form.Group>
+        <Form.Control type="text" name="name" placeholder="Enter your name" value={dataForm.name} onChange={handleChange}/></Form.Group>
 
       <Form.Group controlId="age" className="mb-3">
-        <Form.Control type="number" name="age" placeholder="Enter your age"/>
-      </Form.Group>
+        <Form.Control type="number" name="age" placeholder="Enter your age" value={dataForm.age} onChange={handleChange}/></Form.Group>
 
       <Form.Group controlId="address" className="mb-3">
-        <Form.Control type="text" name="address" placeholder="Enter your address"/>
-      </Form.Group>
+        <Form.Control type="text" name="address" placeholder="Enter your address" value={dataForm.address} onChange={handleChange}/></Form.Group>
 
       <Form.Group controlId="paymentMethod" className="mb-3">
         <Form.Label className={styles.text}>Método de Pago</Form.Label>
-        <Form.Select name="paymentMethod">
-          <option>Cash</option>
-          <option>Transfer</option>
-          <option>Card</option>
+        <Form.Select name="paymentMethod" value={dataForm.paymentMethod} onChange={handleChange}>
+          <option value="cash">Cash</option>
+          <option value="transfer">Transfer</option>
+          <option value="card">Card</option>
         </Form.Select>
       </Form.Group>
-      <Button className={styles.button_cart} variant='success' onClick={clearCart}>Buy</Button>
+      <Button className={styles.button_cart} variant="success" onClick={clearCart}>Buy</Button>
     </Form>
-  );
+  )
 }
 
 export default FormDate
